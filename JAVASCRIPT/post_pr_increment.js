078c905ec3aa011e3cf0159d6d1909ccb3c8ecd1
Sujit <sujitgupta9850@gmail.com>
2024-06-20 18:51:11 +0530

    20-6-24

---
+++ JAVASCRIPT/post_pr_increment.js
@@ -0,0 +1,19 @@
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+
+// }
+
+
+// for(let i=0;i<=5;++i){
+//     console.log(i);
+
+// }
+
+
+let a=1;
+let b=a++;//thik hai pehle b ko value assign karlo phir baad me increment karlo
+console.table({a,b})
+
+// a=1;
+// b=++a;//thik hai pehle a ke value ko assign karlo and then ussey assign karo b ko 
+// console.table({a,b})
